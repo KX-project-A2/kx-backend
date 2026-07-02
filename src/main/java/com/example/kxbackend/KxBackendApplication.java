9b593365804744aa7765c0c70c4137615cf28bcb
yushinKim <dbtls0218@naver.com>
2026-07-02 09:38:34 +0900

    first commit

---
+++ src/main/java/com/example/kxbackend/KxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kxbackend/KxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
